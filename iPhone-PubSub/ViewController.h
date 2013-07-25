a1b2dde2f637fedcb7d733e76bdc259b43b7c169
David Black <dtinblack@gmail.com>
2013-07-25 19:27:04 +0100

    Simple Scroll Interface

--- iPhone-PubSub/ViewController.h
+++ iPhone-PubSub/ViewController.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDataSource,
+                            UITableViewDelegate,UITextFieldDelegate> {
+                                
+            NSMutableArray *messageArray;
+    
+}
+
+@property(weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property(weak, nonatomic) IBOutlet UITableView *messageTable;
 
 @end
--- iPhone-PubSub/ViewController.m
+++ iPhone-PubSub/ViewController.m
@@ -14,16 +14,63 @@
 
 @implementation ViewController
 
+@synthesize scrollView;
+@synthesize messageTable;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    // Initialise the arrays
+    
+    messageArray = [[NSMutableArray alloc] init];
+
+    
+    // load message array to test interface
+    
+    [messageArray addObject:@"First Line"];
+    [messageArray addObject:@"Second Line"];
+    [messageArray addObject:@"Third Line"];
+    [messageArray addObject:@"Fourth Line"];
+       
+    
+    CGSize scrollContentSize = self.view.frame.size;
+    self.scrollView.contentSize = scrollContentSize;
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
+
+
+#pragma mark - Table View
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [messageArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell= [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%@",[messageArray objectAtIndex:indexPath.row]];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Topic: %@",[messageArray objectAtIndex:indexPath.row]];
+    return cell;
+}
+
+
+
 
 @end
